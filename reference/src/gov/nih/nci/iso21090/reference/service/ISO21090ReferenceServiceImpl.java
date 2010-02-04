49c798322da96ec03bf89725715066b35242f176
evanselaj <unknown>
2010-02-04 21:30:41 +0000

    Adding initial skeleton for iso-datatypes reference implementation.
    SVN-Revision: 37

---
+++ reference/src/gov/nih/nci/iso21090/reference/service/ISO21090ReferenceServiceImpl.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.iso21090.reference.service;
+
+import java.rmi.RemoteException;
+
+/** 
+ * TODO:I am the service side implementation class.  IMPLEMENT AND DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class ISO21090ReferenceServiceImpl extends ISO21090ReferenceServiceImplBase {
+
+	
+	public ISO21090ReferenceServiceImpl() throws RemoteException {
+		super();
+	}
+	
+}
+
