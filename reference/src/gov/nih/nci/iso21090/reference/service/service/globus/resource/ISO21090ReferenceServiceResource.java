eea3ec6ebe80d69770ff4d3dd91cbaef906ff195
evanselaj <unknown>
2010-02-08 19:31:46 +0000

    Re-adding skeleton of project.
    SVN-Revision: 49

---
+++ reference/src/gov/nih/nci/iso21090/reference/service/service/globus/resource/ISO21090ReferenceServiceResource.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.iso21090.reference.service.service.globus.resource;
+
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.ResourceException;
+import org.globus.wsrf.ResourceKey;
+
+
+/** 
+ * The implementation of this ISO21090ReferenceServiceResource type.
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class ISO21090ReferenceServiceResource extends ISO21090ReferenceServiceResourceBase {
+
+}
