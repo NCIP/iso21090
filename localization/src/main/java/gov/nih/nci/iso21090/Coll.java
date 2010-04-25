c5704bba691b84e102e2fe65a3aac9c3e18f6f80
Satish Patel <patelsat@mail.nih.gov>
2010-04-25 04:34:06 +0000

    Added Type parameter to Coll class as per specification
    SVN-Revision: 143

--- localization/src/main/java/gov/nih/nci/iso21090/Coll.java
+++ localization/src/main/java/gov/nih/nci/iso21090/Coll.java
@@ -85,6 +85,8 @@ package gov.nih.nci.iso21090;
 /**
  * Represents collection of ANY.
  * @author Dan Dumitru
+ * 
+ * @param <T> Collection element type
  */
 @SuppressWarnings({ "PMD.AbstractNaming", "PMD.AbstractClassWithoutAnyMethod" })
 public abstract class Coll<T extends Any> extends Any implements Cloneable {
