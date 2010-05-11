fa34a0cf627c3ba1b021583a16caf4e346fb7344
Justin Permar <justin.permar@osumc.edu>
2010-05-11 18:17:11 +0000

    Added updated extensions jar with the Filter fix for https://gforge.nci.nih.gov/tracker/?func=detail&aid=28474&group_id=25&atid=174. The fix is simply to skip the attribute that the Filter is modifying if a namespace can't be found (e.g., no "xsi" in "xsi:type" for that attribute).
    It's producing incorrect XML (see xsi:type):
    
    <ns1:Ad controlActExtension="123" xsi:type="ns1:Ad" xmlns:ns1="uri:iso.org:21090" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><ns1:useablePeriod xsi:type="xsi:xsi:type"/></ns1:Ad>
    
    Fix of Filter.java now produces correct XML: 
    
    <ns1:Ad controlActExtension="123" xsi:type="ns1:Ad" xmlns:ns1="uri:iso.org:21090" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><ns1:useablePeriod xsi:type="IVL_TS"/></ns1:Ad>
    
    
    SVN-Revision: 211

--- code/extensions/src/main/java/gov/nih/nci/iso21090/grid/ser/Filter.java
+++ code/extensions/src/main/java/gov/nih/nci/iso21090/grid/ser/Filter.java
@@ -103,136 +103,142 @@ import org.xml.sax.helpers.AttributesImpl;
  * @author gax
  */
 public class Filter implements ContentHandler {
-    private final SerializationContext context;
+	private final SerializationContext context;
 
-    private final Stack<Map<String, String>> prefixes = new Stack<Map<String, String>>();
-    private Map<String, String> head = new HashMap<String, String>();
+	private final Stack<Map<String, String>> prefixes = new Stack<Map<String, String>>();
+	private Map<String, String> head = new HashMap<String, String>();
 
-    /**
-     * Constructs a new filter, based on the given context.
-     * @param context serialization context to use
-     */
-    public Filter(SerializationContext context) {
-        this.context = context;
-    }
+	/**
+	 * Constructs a new filter, based on the given context.
+	 * @param context serialization context to use
+	 */
+	public Filter(SerializationContext context) {
+		this.context = context;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void startPrefixMapping(String prefix, String uri) throws SAXException {
-        head.put(prefix, uri);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void startPrefixMapping(String prefix, String uri) throws SAXException {
+		head.put(prefix, uri);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void endPrefixMapping(String prefix) throws SAXException {
-        head.remove(prefix);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void endPrefixMapping(String prefix) throws SAXException {
+		head.remove(prefix);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+	/**
+	 * {@inheritDoc}
+	 */
+	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
 
-        prefixes.push(head);
-        head = new HashMap<String, String>();
+		prefixes.push(head);
+		head = new HashMap<String, String>();
 
-        AttributesImpl fixed = new AttributesImpl();
-        for (int i = 0; i < atts.getLength(); i++) {
-            String au = atts.getURI(i);
-            String av = atts.getValue(i);
-            String aln = atts.getLocalName(i);
-            if (NamespaceConstants.NSURI_SCHEMA_XSI.equals(au) && "type".equals(aln)) {
-                int idx = av.indexOf(':');
-                String p = av.substring(0, idx);
-                String q = av.substring(av.indexOf(':') + 1);
-                String ns = getNS(p);
-                String axisPrefix = context.getPrefixForURI(ns);
-                av = axisPrefix + ':' + q;
+		AttributesImpl fixed = new AttributesImpl();
+		for (int i = 0; i < atts.getLength(); i++) {
+			String au = atts.getURI(i);
+			String av = atts.getValue(i);
+			String aln = atts.getLocalName(i);
+			String aq = atts.getQName(i);
+			if (NamespaceConstants.NSURI_SCHEMA_XSI.equals(au) && "type".equals(aln)) {
+				int idx = av.indexOf(':');
+				if (idx != -1) {
+					String p = av.substring(0, idx);
+					String q = av.substring(av.indexOf(':') + 1);
+					//					int idx = aq.indexOf(':');
+					//					String p = aq.substring(0, idx);
+					//					String q = aq.substring(av.indexOf(':') + 1);
+					String ns = getNS(p);
+					String axisPrefix = context.getPrefixForURI(ns);
+					av = axisPrefix + ':' + q;
+				}
 
-            }
-            fixed.addAttribute(au, aln, atts.getQName(i), atts.getType(i), av);
-        }
+			}
+			fixed.addAttribute(au, aln, atts.getQName(i), atts.getType(i), av);
+		}
 
-        try {
-            context.startElement(new QName(uri, localName), fixed);
-        } catch (IOException ioe) {
-            throw new SAXException(ioe);
-        }
-    }
+		try {
+			context.startElement(new QName(uri, localName), fixed);
+		} catch (IOException ioe) {
+			throw new SAXException(ioe);
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-        head = prefixes.pop();
-        try {
-            context.endElement();
-        } catch (IOException ioe) {
-            throw new SAXException(ioe);
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		head = prefixes.pop();
+		try {
+			context.endElement();
+		} catch (IOException ioe) {
+			throw new SAXException(ioe);
+		}
+	}
 
-    private String getNS(String prefix) {
-        String ns = null;
-        for (int i = prefixes.size() - 1; i >= 0 && ns == null; i--) {
-            ns = prefixes.get(i).get(prefix);
-        }
-        return ns;
-    }
+	private String getNS(String prefix) {
+		String ns = null;
+		for (int i = prefixes.size() - 1; i >= 0 && ns == null; i--) {
+			ns = prefixes.get(i).get(prefix);
+		}
+		return ns;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        try {
-            context.writeChars(ch, start, length);
-        } catch (IOException ioe) {
-            throw new SAXException(ioe);
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		try {
+			context.writeChars(ch, start, length);
+		} catch (IOException ioe) {
+			throw new SAXException(ioe);
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void setDocumentLocator(Locator locator) {
-        //
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setDocumentLocator(Locator locator) {
+		//
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void startDocument() throws SAXException {
-        //
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void startDocument() throws SAXException {
+		//
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void endDocument() throws SAXException {
-        //
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void endDocument() throws SAXException {
+		//
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
-        //
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+		//
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void processingInstruction(String target, String data) throws SAXException {
-        //
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void processingInstruction(String target, String data) throws SAXException {
+		//
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void skippedEntity(String name) throws SAXException {
-        //
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void skippedEntity(String name) throws SAXException {
+		//
+	}
 
 }
