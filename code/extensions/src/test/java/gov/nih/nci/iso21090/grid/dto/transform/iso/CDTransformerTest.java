10506f4df7054460999ba858344e8e8f5e851aed
Vijay Parmar <parmarv@mail.nih.gov>
2010-08-12 22:17:06 +0000

    SVN-Revision: 314

--- code/extensions/src/test/java/gov/nih/nci/iso21090/grid/dto/transform/iso/CDTransformerTest.java
+++ code/extensions/src/test/java/gov/nih/nci/iso21090/grid/dto/transform/iso/CDTransformerTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertNull;
 import gov.nih.nci.iso21090.Cd;
 import gov.nih.nci.iso21090.NullFlavor;
 import gov.nih.nci.iso21090.grid.dto.transform.AbstractTransformerTestBase;
-import gov.nih.nci.iso21090.grid.dto.transform.iso.CDTransformer.CDCoreTransformer;
+
 
 import org.iso._21090.CD;
 import org.junit.Test;
@@ -15,7 +15,7 @@ import org.junit.Test;
  *
  * @author gax
  */
-public class CDTransformerTest extends AbstractTransformerTestBase<CDCoreTransformer, CD, Cd> {
+public class CDTransformerTest extends AbstractTransformerTestBase<CDTransformer, CD, Cd> {
 
     @Override
     public Cd makeDtoSimple() {
@@ -64,12 +64,12 @@ public class CDTransformerTest extends AbstractTransformerTestBase<CDCoreTransfo
     public void testCdCodeNull() throws Exception {
         Cd cd = new Cd();
         cd.setNullFlavor(NullFlavor.ASKU);
-        CD result = CDCoreTransformer.INSTANCE.toXml(cd);
+        CD result = CDTransformer.INSTANCE.toXml(cd);
         assertNotNull(result);
         assertNull(result.getCode());
         assertEquals(org.iso._21090.NullFlavor.ASKU, result.getNullFlavor());
 
-        cd = CDCoreTransformer.INSTANCE.toDto(result);
+        cd = CDTransformer.INSTANCE.toDto(result);
         assertNotNull(cd);
         assertNull(cd.getCode());
         assertEquals(NullFlavor.ASKU, cd.getNullFlavor());
