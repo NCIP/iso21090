2e7539d3292132419f5e7c4e23fc1eb39823c4c7
Vijay Parmar <parmarv@mail.nih.gov>
2010-08-12 22:02:06 +0000

    SVN-Revision: 311

--- code/extensions/src/test/java/gov/nih/nci/iso21090/grid/dto/transform/iso/CDCVTransformerTest.java
+++ code/extensions/src/test/java/gov/nih/nci/iso21090/grid/dto/transform/iso/CDCVTransformerTest.java
@@ -6,17 +6,18 @@ import static org.junit.Assert.assertNull;
 import gov.nih.nci.iso21090.Cd;
 import gov.nih.nci.iso21090.NullFlavor;
 import gov.nih.nci.iso21090.grid.dto.transform.AbstractTransformerTestBase;
-import gov.nih.nci.iso21090.grid.dto.transform.iso.CDTransformer.CDCoreTransformer;
+import gov.nih.nci.iso21090.grid.dto.transform.iso.CDTransformer.CdTransformer;
 
-import org.iso._21090.CD;
+import org.iso._21090.Cd;
 import org.junit.Test;
 
 /**
  *
- * @author gax
+ * @author Vijay Parmar
  */
-public class CDCVTransformerTest extends AbstractTransformerTestBase<CDCoreTransformer, CD, Cd> {
+public class CdTransformerTest extends AbstractTransformerTestBase<CdTransformer, Cd, Cd> {
 
+	
     @Override
     public Cd makeDtoSimple() {
         Cd i = new Cd();
@@ -30,8 +31,8 @@ public class CDCVTransformerTest extends AbstractTransformerTestBase<CDCoreTrans
     }
 
     @Override
-    public CD makeXmlSimple() {
-        CD x = new CD();
+    public Cd makeXmlSimple() {
+        Cd x = new Cd();
         x.setCode("code");
         x.setCodeSystem("sys");
         x.setCodeSystemName("name");
@@ -44,7 +45,7 @@ public class CDCVTransformerTest extends AbstractTransformerTestBase<CDCoreTrans
     }
 
     @Override
-    public void verifyXmlSimple(CD x) {
+    public void verifyXmlSimple(Cd x) {
         assertEquals("code", x.getCode());
         assertEquals("ignored", x.getCodeSystem());
         assertEquals("ignored", x.getCodeSystemName());
@@ -64,12 +65,12 @@ public class CDCVTransformerTest extends AbstractTransformerTestBase<CDCoreTrans
     public void testCdCodeNull() throws Exception {
         Cd cd = new Cd();
         cd.setNullFlavor(NullFlavor.ASKU);
-        CD result = CDCoreTransformer.INSTANCE.toXml(cd);
+        Cd result = CdTransformer.INSTANCE.toXml(cd);
         assertNotNull(result);
         assertNull(result.getCode());
         assertEquals(org.iso._21090.NullFlavor.ASKU, result.getNullFlavor());
 
-        cd = CDCoreTransformer.INSTANCE.toDto(result);
+        cd = CdTransformer.INSTANCE.toDto(result);
         assertNotNull(cd);
         assertNull(cd.getCode());
         assertEquals(NullFlavor.ASKU, cd.getNullFlavor());
