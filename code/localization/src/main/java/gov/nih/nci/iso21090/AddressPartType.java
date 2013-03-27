9e68c56319ce1d8b512cdf59dd560c38ca6bcaf7
U-NIH\konkapv <konkapv@mail.nih.gov>
2013-03-27 12:03:44 -0400

    Added Copyright, README, LICENSE, NOTICE

--- code/localization/src/main/java/gov/nih/nci/iso21090/AddressPartType.java
+++ code/localization/src/main/java/gov/nih/nci/iso21090/AddressPartType.java
@@ -1,3 +1,14 @@
+//============================================================================
+// Copyright 5AM Solutions, Inc
+// Copyright Ekagra Software Technologies Ltd.
+// Copyright Guidewire Architecture
+// Copyright The Ohio State University Research Foundation
+// Copyright Science Applications International Corporation
+//
+// Distributed under the OSI-approved BSD 3-Clause License.
+// See http://ncip.github.com/iso21090/LICENSE.txt for details.
+//============================================================================
+
 /**
  * The software subject to this notice and license includes both human readable
  * source code form and machine readable, binary, object code form. The iso-datatypes
