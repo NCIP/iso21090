b01780ab0f8f393a6583f0e551a652b230c89262
U-NIH\konkapv <konkapv@mail.nih.gov>
2013-03-27 12:00:48 -0400

    Added Copyright, README, LICENSE, NOTICE

--- code/extensions/src/main/java/gov/nih/nci/iso21090/grid/ser/JaxbContextManager.java
+++ code/extensions/src/main/java/gov/nih/nci/iso21090/grid/ser/JaxbContextManager.java
@@ -1,3 +1,14 @@
+//============================================================================
+// Copyright 5AM Solutions, Inc
+// Copyright Ekagra Software Technologies Ltd.
+// Copyright Guidewire Architecture
+// Copyright The Ohio State University Research Foundation
+// Copyright Science Applications International Corporation
+//
+// Distributed under the OSI-approved BSD 3-Clause License.
+// See http://ncip.github.com/iso21090/LICENSE.txt for details.
+//============================================================================
+
 package gov.nih.nci.iso21090.grid.ser;
 
 import java.util.Collections;
